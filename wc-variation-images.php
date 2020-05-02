d7c34ad047e1c92c27e8882e899b0e0ab20e3dd9
Sultan Nasir Uddin <manikdrmc@gmail.com>
2020-05-02 22:42:56 +0600

    functions rewrite

--- wc-variation-images.php
+++ wc-variation-images.php
@@ -52,184 +52,68 @@ final class WC_Variation_Images {
 	public $version = '1.0.1';
 
 	/**
-	 * Minimum PHP version required
-	 *
-	 * @var string
-	 */
-	private $min_php = '5.6.0';
-
-	/**
-	 * admin notices
-	 *
-	 * @since 1.0.0
-	 *
-	 * @var array
-	 */
-	protected $notices = array();
-
-	/**
 	 * The single instance of the class.
 	 *
 	 * @since 1.0.0
 	 *
 	 * @var WC_Variation_Images
 	 */
-	protected static $instance = null;
+	protected static $_instance = null;
 
 	/**
-	 * @since 1.0.0
+	 * Main Plugin Instance.
 	 *
-	 * @var string
+	 * Ensures only one instance of plugin is loaded or can be loaded.
+	 *
+	 * @return WC_Variation_Images - Main instance.
+	 * @since 1.0.0
+	 * @static
 	 */
-	public $plugin_name = 'Woocommerce Variation Images';
+	public static function instance() {
+		if ( is_null( self::$_instance ) ) {
+			self::$_instance = new self();
+		}
+
+		return self::$_instance;
+	}
+
+	/**
+	 * Cloning is forbidden.
+	 *
+	 * @return void
+	 * @since 1.0.0
+	 */
+	public function __clone() {
+		_doing_it_wrong( __FUNCTION__, __( 'Cloning is forbidden.', 'wc-variation-images' ), '1.0.0' );
+	}
+
+	/**
+	 * Universalizing instances of this class is forbidden.
+	 *
+	 * @return void
+	 * @since 1.0.0
+	 */
+	public function __wakeup() {
+		_doing_it_wrong( __FUNCTION__, __( 'Unserializing instances of this class is forbidden.', 'wc-variation-images' ), '1.0.0' );
+	}
+
 
 	/**
 	 * WC_Variation_Images constructor.
 	 */
-	public function __construct() {
-		register_activation_hook( __FILE__, array( $this, 'activation_check' ) );
-		add_action( 'admin_notices', array( $this, 'admin_notices' ), 15 );
-		add_action( 'init', array( $this, 'localization_setup' ) );
-		add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), array( $this, 'plugin_action_links' ) );
-		if ( $this->is_plugin_compatible() ) {
-			$this->define_constants();
-			$this->includes();
-			do_action( 'wc_variation_images_loaded' );
-		}
-	}
-
-	/**
-	 * Checks the server environment and other factors and deactivates plugins as necessary.
-	 *
-	 * @internal
-	 *
-	 * @since 1.0.0
-	 */
-	public function activation_check() {
-
-		if ( ! version_compare( PHP_VERSION, $this->min_php, '>=' ) ) {
-
-			deactivate_plugins( plugin_basename( __FILE__ ) );
-
-			$message = sprintf( '%s could not be activated The minimum PHP version required for this plugin is %1$s. You are running %2$s.', $this->plugin_name, $this->min_php, PHP_VERSION );
-			wp_die( $message );
-		}
-
-	}
-
-	/**
-	 * Displays any admin notices added
-	 *
-	 * @internal
-	 *
-	 * @since 1.0.0
-	 */
-	public function admin_notices() {
-		$notices = (array) array_merge( $this->notices, get_option( sanitize_key( $this->plugin_name ), [] ) );
-		foreach ( $notices as $notice_key => $notice ) :
-			?>
-			<div class="notice notice-<?php echo sanitize_html_class( $notice['class'] ); ?>">
-				<p><?php echo wp_kses( $notice['message'], array(
-						'a'      => array( 'href' => array() ),
-						'strong' => array()
-					) ); ?></p>
-			</div>
-			<?php
-			update_option( sanitize_key( $this->plugin_name ), [] );
-		endforeach;
-	}
-
-	/**
-	 * Initialize plugin for localization
-	 *
-	 * @since 1.0.0
-	 *
-	 * @return void
-	 */
-	public function localization_setup() {
-		load_plugin_textdomain( 'wc-variation-images', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
-	}
-
-
-	/**
-	 * Determines if the pro version installed.
-	 *
-	 * @since 1.0.0
-	 *
-	 * @return bool
-	 */
-	public function is_pro_installed() {
-		return is_plugin_active( 'wc-variation-images-pro/wc-variation-images-pro.php' ) == true;
-	}
-
-	/**
-	 * Plugin action links
-	 *
-	 * @param  array $links
-	 *
-	 * @return array
-	 */
-	public function plugin_action_links( $links ) {
-		$links[] = '<a href="https://www.pluginever.com/docs/wc-variation-images/">' . __( 'Documentation', 'wc-variation-images' ) . '</a>';
-		if ( ! $this->is_pro_installed() ) {
-			$links[] = '<a href="https://www.pluginever.com/plugins/wc-variation-images-pro/?utm_source=plugin_action_link&utm_medium=link&utm_campaign=wc-variation-images&utm_content=Upgrade%20to%20Pro" style="color: red;font-weight: bold;" target="_blank">' . __( 'Upgrade to PRO', 'wc-variation-images' ) . '</a>';
-		}
-
-		return $links;
-	}
-
-	/**
-	 * Determines if the plugin compatible.
-	 *
-	 * @since 1.0.0
-	 *
-	 * @return bool
-	 */
-	protected function is_plugin_compatible() {
-		include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
-		if ( ! is_plugin_active( 'woocommerce/woocommerce.php' ) ) {
-			$this->add_notice( 'error', sprintf(
-				'<strong>%s</strong> requires <strong>WooCommerce</strong> installed and active.',
-				$this->plugin_name
-			) );
-
-			return false;
-		}
-
-		return true;
-	}
-
-	/**
-	 * Adds an admin notice to be displayed.
-	 *
-	 * @since 1.0.0
-	 *
-	 * @param string $class the notice class
-	 * @param string $message the notice message body
-	 */
-	public function add_notice( $class, $message ) {
-
-		$notices = get_option( sanitize_key( $this->plugin_name ), [] );
-		if ( is_string( $message ) && is_string( $class ) && ! wp_list_filter( $notices, array( 'message' => $message ) ) ) {
-
-			$notices[] = array(
-				'message' => $message,
-				'class'   => $class
-			);
-
-			update_option( sanitize_key( $this->plugin_name ), $notices );
-		}
-
+	protected function __construct() {
+		$this->define_constants();
+		$this->includes();
+		$this->init_hooks();
 	}
 
 	/**
 	 * Define EverProjects Constants.
 	 *
-	 * @since 1.0.0
 	 * @return void
+	 * @since 1.0.0
 	 */
 	private function define_constants() {
-
 		define( 'WC_VARIATION_IMAGES_VERSION', $this->version );
 		define( 'WC_VARIATION_IMAGES_FILE', __FILE__ );
 		define( 'WC_VARIATION_IMAGES_PATH', dirname( WC_VARIATION_IMAGES_FILE ) );
@@ -238,6 +122,7 @@ final class WC_Variation_Images {
 		define( 'WC_VARIATION_IMAGES_ASSETS_URL', WC_VARIATION_IMAGES_URL . '/assets' );
 		define( 'WC_VARIATION_IMAGES_TEMPLATES_DIR', WC_VARIATION_IMAGES_PATH . '/templates' );
 	}
+
 
 	/**
 	 * Include required core files used in admin and on the frontend.
@@ -256,6 +141,69 @@ final class WC_Variation_Images {
 			require_once( WC_VARIATION_IMAGES_INCLUDES . '/admin/class-settings.php' );
 		}
 
+	}
+
+	/**
+	 * Hook into actions and filters.
+	 *
+	 * @since 1.0.0
+	 */
+	private function init_hooks() {
+		register_activation_hook( __FILE__, array( __CLASS__, 'install' ) );
+		add_action( 'init', array( __CLASS__, 'localization_setup' ) );
+		add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), array( $this, 'plugin_action_links' ) );
+	}
+
+	/**
+	 * setup necessary settings data
+	 *
+	 * @internal
+	 *
+	 * @since 1.0.0
+	 */
+	public static function install() {
+		include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+		if ( ! is_plugin_active( 'woocommerce/woocommerce.php' ) ) {
+			// deactivate plugin
+		}
+	}
+
+	/**
+	 * Initialize plugin for localization
+	 *
+	 * @return void
+	 * @since 1.0.0
+	 *
+	 */
+	public static function localization_setup() {
+		load_plugin_textdomain( 'wc-variation-images', false, dirname( plugin_basename( __FILE__ ) ) . '/i18n/languages/' );
+	}
+
+	/**
+	 * Determines if the pro version installed.
+	 *
+	 * @return bool
+	 * @since 1.0.0
+	 *
+	 */
+	public static function is_pro_installed() {
+		return is_plugin_active( 'wc-variation-images-pro/wc-variation-images-pro.php' ) == true;
+	}
+
+	/**
+	 * Plugin action links
+	 *
+	 * @param array $links
+	 *
+	 * @return array
+	 */
+	public function plugin_action_links( $links ) {
+		$links[] = '<a href="https://www.pluginever.com/docs/wc-variation-images/">' . __( 'Documentation', 'wc-variation-images' ) . '</a>';
+		if ( ! $this->is_pro_installed() ) {
+			$links[] = '<a href="https://www.pluginever.com/plugins/wc-variation-images-pro/?utm_source=plugin_action_link&utm_medium=link&utm_campaign=wc-variation-images&utm_content=Upgrade%20to%20Pro" style="color: red;font-weight: bold;" target="_blank">' . __( 'Upgrade to PRO', 'wc-variation-images' ) . '</a>';
+		}
+
+		return $links;
 	}
 
 	/**
@@ -283,23 +231,6 @@ final class WC_Variation_Images {
 	 */
 	public function template_path() {
 		return WC_VARIATION_IMAGES_TEMPLATES_DIR;
-	}
-
-	/**
-	 * Main WC_Variation_Images Instance.
-	 *
-	 * Ensures only one instance of WC_Variation_Images is loaded or can be loaded.
-	 *
-	 * @since 1.0.0
-	 * @static
-	 * @return WC_Variation_Images - Main instance.
-	 */
-	public static function instance() {
-		if ( is_null( self::$instance ) ) {
-			self::$instance = new self();
-		}
-
-		return self::$instance;
 	}
 
 }
