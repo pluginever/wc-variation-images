349102fe28144dd43b4b4ce26cc4f6485ac1a95a
devsabbirhossain <sabbir2dev@gmail.com>
2024-10-23 14:49:07 +0600

    add footer text

--- includes/Admin/Admin.php
+++ includes/Admin/Admin.php
@@ -31,6 +31,8 @@ class Admin {
 		add_action( 'admin_menu', array( $this, 'admin_menu' ) );
 		add_action( 'woocommerce_save_product_variation', array( $this, 'wc_variation_images_save_product_variation' ), 10, 1 );
 		add_action( 'admin_footer', array( $this, 'admin_template_js' ) );
+		add_filter( 'admin_footer_text', array( $this, 'admin_footer_text' ), PHP_INT_MAX );
+		add_filter( 'update_footer', array( $this, 'update_footer' ), PHP_INT_MAX );
 	}
 
 	/**
@@ -202,5 +204,43 @@ class Admin {
 			$attachment_ids = array_unique( $attachment_ids );
 		}
 		update_post_meta( $variation_id, 'wc_variation_images_variation_images', $attachment_ids );
+	}
+
+	/**
+	 * Admin footer text.
+	 *
+	 * @param string $footer_text Footer text.
+	 *
+	 * @since 1.1.1
+	 * @return string
+	 */
+	public function admin_footer_text( $footer_text ) {
+		if ( wc_variation_images()->get_review_url() && in_array( get_current_screen()->id, array( 'toplevel_page_wc-variation-images' ), true ) ) {
+			$footer_text = sprintf(
+			/* translators: 1: Plugin name 2: WordPress */
+				__( 'Thank you for using %1$s. If you like it, please leave us a %2$s rating. A huge thank you from PluginEver in advance!', 'wc-variation-images' ),
+				'<strong>' . esc_html( wc_variation_images()->get_name() ) . '</strong>',
+				'<a href="' . esc_url( wc_variation_images()->get_review_url() ) . '" target="_blank" class="wc-variation-images-rating-link" data-rated="' . esc_attr__( 'Thanks :)', 'wc-variation-images' ) . '">&#9733;&#9733;&#9733;&#9733;&#9733;</a>'
+			);
+		}
+
+		return $footer_text;
+	}
+
+	/**
+	 * Update footer.
+	 *
+	 * @param string $footer_text Footer text.
+	 *
+	 * @since 1.1.1
+	 * @return string
+	 */
+	public function update_footer( $footer_text ) {
+		if ( in_array( get_current_screen()->id, array( 'toplevel_page_wc-variation-images' ), true ) ) {
+			/* translators: 1: Plugin version */
+			$footer_text = sprintf( esc_html__( 'Version %s', 'wc-variation-images' ), wc_variation_images()->get_version() );
+		}
+
+		return $footer_text;
 	}
 }
