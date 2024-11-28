3c713ba7471e196c4f5b90553ac7bf41b9314132
devsabbirhossain <sabbir2dev@gmail.com>
2024-11-28 12:48:19 +0600

    cleanup files

--- webpack.config.js
+++ webpack.config.js
@@ -7,11 +7,11 @@ module.exports = [
 		...defaultConfig,
 		entry: {
 			...defaultConfig.entry(),
-			'css/halloween': './.assets/css/halloween.scss',
-			'css/admin': './.assets/css/admin.scss',
-			'css/frontend': './.assets/css/frontend.scss',
-			'js/admin': './.assets/js/admin.js',
-			'js/frontend': './.assets/js/frontend.js',
+			'css/halloween': './assets/css/halloween.scss',
+			'css/admin': './assets/css/admin.scss',
+			'css/frontend': './assets/css/frontend.scss',
+			'js/admin': './assets/js/admin.js',
+			'js/frontend': './assets/js/frontend.js',
 		},
 		output: {
 			...defaultConfig.output,
@@ -37,8 +37,8 @@ module.exports = [
 			new CopyWebpackPlugin({
 				patterns: [
 					{
-						from: path.resolve(__dirname, '.assets/images'),
-						to: path.resolve(__dirname, 'assets/images'),
+						from: path.resolve(__dirname, 'assets/images'),
+						to: path.resolve(__dirname, 'build/images'),
 					},
 				]
 			}),
