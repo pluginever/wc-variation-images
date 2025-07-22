490755aae139ea91578430852fc70ee29e1489ae
kawsarahmedr <kawsar.ahmed198133@gmail.com>
2025-07-22 15:20:03 +0600

    Add go pro btn in plugin action links

--- includes/Plugin.php
+++ includes/Plugin.php
@@ -61,6 +61,7 @@ final class Plugin extends \WooCommerceVariationImages\ByteKit\Plugin {
 	 */
 	public function init_hooks() {
 		register_activation_hook( $this->get_file(), array( $this, 'install' ) );
+		add_filter( 'plugin_action_links_' . $this->get_basename(), array( $this, 'plugin_action_links' ) );
 		add_action( 'before_woocommerce_init', array( $this, 'on_before_woocommerce_init' ) );
 		add_action( 'admin_notices', array( $this, 'dependencies_notices' ) );
 		add_action( 'woocommerce_init', array( $this, 'init' ), 0 );
@@ -76,6 +77,22 @@ final class Plugin extends \WooCommerceVariationImages\ByteKit\Plugin {
 	public function install() {
 		// Add option for installed time.
 		add_option( 'wc_variation_images_installed', wp_date( 'U' ) );
+	}
+
+	/**
+	 * Add plugin action links.
+	 *
+	 * @param array $links The plugin action links.
+	 *
+	 * @since 1.0.0
+	 * @return array
+	 */
+	public function plugin_action_links( $links ) {
+		if ( ! $this->is_plugin_active( 'wc-variation-images-pro/wc-variation-images-pro.php' ) ) {
+			$links['go_pro'] = '<a href="https://pluginever.com/plugins/wc-variation-images-pro" target="_blank" style="color: #39b54a; font-weight: bold;">' . esc_html__( 'Go Pro', 'wc-variation-images' ) . '</a>';
+		}
+
+		return $links;
 	}
 
 	/**
