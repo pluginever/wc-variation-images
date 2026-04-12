0235492b0c3c830edbf2c23006bc1b861892be26
SH Sajal Chowdhury <sajal.marketer@gmail.com>
2026-04-12 10:39:23 +0600

    Fix missing fancybox and slider assets in build folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 /**
  * External dependencies
  */
+const path = require( 'path' );
+const CopyWebpackPlugin = require( 'copy-webpack-plugin' );
 const baseConfig = require( '@byteever/scripts/config/webpack.config' );
 
 module.exports = {
@@ -12,4 +14,21 @@ module.exports = {
 		'js/admin': './assets/src/js/admin.js',
 		'js/frontend': './assets/src/js/frontend.js'
 	},
+	plugins: [
+		...baseConfig.plugins,
+		new CopyWebpackPlugin( {
+			patterns: [
+				{
+					from: path.resolve( __dirname, 'assets/src/js/slider' ),
+					to: path.resolve( __dirname, 'assets/build/js' ),
+					noErrorOnMissing: true,
+				},
+				{
+					from: path.resolve( __dirname, 'assets/src/css/slider' ),
+					to: path.resolve( __dirname, 'assets/build/css' ),
+					noErrorOnMissing: true,
+				},
+			],
+		} ),
+	],
 };
